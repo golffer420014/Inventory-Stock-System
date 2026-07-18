bea918c28f88528316519e0a65a3aad313d8945a
suriya <142491843+golffer420014@users.noreply.github.com>
2026-07-18 16:33:46 +0700

    update

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,7 @@
 import cors from 'cors'
 import express from 'express'
+import helmet from 'helmet'
+import morgan from 'morgan'
 import { env } from '@/config/env.js'
 import { errorHandlerMiddleware } from '@/middlewares/errorHandler.middleware.js'
 import { authMiddleware } from '@/middlewares/auth.middleware.js'
@@ -7,7 +9,11 @@ import { apiRouter } from '@/routes/index.js'
 
 export const app = express()
 
+app.use(helmet())
 app.use(cors({ origin: env.clientOrigin }))
+if (env.nodeEnv !== 'test') {
+  app.use(morgan('dev'))
+}
 app.use(express.json())
 app.use(authMiddleware)
 
