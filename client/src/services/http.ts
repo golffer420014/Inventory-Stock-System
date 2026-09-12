2a274780b4bea886d53108d6d0c70b770b48f6b5
suriya <isupermax.11@gmail.com>
2026-09-12 19:20:36 +0700

    client deploy

--- client/src/services/http.ts
+++ client/src/services/http.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { useRoleStore } from '@/stores/role'
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000/api'
+const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api`
 
 const client = axios.create({ baseURL: API_BASE_URL })
 
